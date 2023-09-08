5c15258553b2c19ff2f78bcd545553ea496eaf82
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-09-08 11:20:36 -0500

    Update 9/5

--- src/Quarter1/Prog58h.java
+++ src/Quarter1/Prog58h.java
@@ -4,5 +4,45 @@ import java.util.*;
 public class Prog58h {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the amount saved? ");
+        int p = input.nextInt(); // amount originally saved
+
+        System.out.print("What is the interest rate? ");
+        double r = input.nextDouble(); // rate of interest in percent
+
+        System.out.print("What is the number of times compounded per year? ");
+        int n = input.nextInt(); // number of days compounded per year
+
+        System.out.print("What is the number of days at interest? ");
+        double t = input.nextDouble(); // number of days the money is to be kept in the savings account
+
+        double a = p*(Math.pow(1+((.01*r)/n),(n*t)/365)); // amount
+        double i = a-p; // interest earned
+
+        System.out.printf("The interest earned is $%.2f\n", i);
+        System.out.printf("The total amount in savings is now $%.2f", a);
     }
 }
+/*
+What is the amount saved? 5000
+What is the interest rate? 11.5
+What is the number of times compounded per year? 365
+What is the number of days at interest? 900
+The interest earned is $1638.96
+The total amount in savings is now $6638.96
+
+What is the amount saved? 10000
+What is the interest rate? 16.5
+What is the number of times compounded per year? 365
+What is the number of days at interest? 90
+The interest earned is $415.14
+The total amount in savings is now $10415.14
+
+What is the amount saved? 1500
+What is the interest rate? 8.5
+What is the number of times compounded per year? 365
+What is the number of days at interest? 365
+The interest earned is $133.06
+The total amount in savings is now $1633.06
+ */
--- src/Quarter1/Prog58i.java
+++ src/Quarter1/Prog58i.java
@@ -12,15 +12,15 @@ public class Prog58i {
         double r = input.nextDouble(); // rate
 
         System.out.print("The # of months to pay off the loan is ");
-        double m = input.nextDouble(); //months
+        double m = input.nextDouble(); // months
 
         double r_over_1200 = r/1200;
 
         double mp = p * (r_over_1200 * (Math.pow((1+r_over_1200), m)
                                      / (Math.pow((1+r_over_1200), m)-1)));
         System.out.printf("My monthly payments will be $%.2f\n",mp);
-        System.out.printf("The total interest paid is $%.2f\n", mp + m);
-        System.out.printf("The total amount is $%.2f\n", ((mp * m) + p));
+        System.out.printf("The total interest paid is $%.2f\n", mp * m);
+        System.out.printf("The total amount is $%.2f\n", (mp * m) + p);
     }
 }
 /*
@@ -28,6 +28,6 @@ The amount I wish to borrow is $7500
 The loan rate is 14.5
 The # of months to pay off the loan is 36
 My monthly payments will be $258.16
-The total interest paid is $294.16
+The total interest paid is $9293.66
 The total amount is $16793.66
  */
