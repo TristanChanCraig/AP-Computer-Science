f3681ebadfe7f7fca83afaf963a7a069d0501994
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-09-18 11:24:18 -0500

    Update 9/5

--- src/Quarter1/Prog82a.java
+++ src/Quarter1/Prog82a.java
@@ -11,8 +11,17 @@ public class Prog82a {
         System.out.print("Enter the vehicle speed: ");
         int vS = input.nextInt();
 
-        int fine = 20 + ((vS - sL) * 12);
+        int fine = 20 + ((vS - sL) * 5);
 
-        System.out.print("Fine------------------" + fine + ".00");
+        System.out.print("Fine------------------$" + fine + ".00");
     }
 }
+/*
+Enter the speed limit: 30
+Enter the vehicle speed: 42
+Fine------------------$80.00
+
+Enter the speed limit: 55
+Enter the vehicle speed: 70
+Fine------------------$95.00
+ */
