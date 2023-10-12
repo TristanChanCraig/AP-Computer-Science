828ce244655ee9c716df236a2c07d398fa2ee437
TristanChanCraig <126594566+TristanChanCraig@users.noreply.github.com>
2023-10-12 15:09:19 -0500

    Update Prog88aCl.java

--- src/Quarter1/Prog88aCl/Prog88aCl.java
+++ src/Quarter1/Prog88aCl/Prog88aCl.java
@@ -1,7 +1,20 @@
 package Quarter1.Prog88aCl;
+import java.util.*;
 
 public class Prog88aCl {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a number between 1 and 13");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter a number between 2 and 20");
+        int num2 = input.nextInt();
+
+        Cl88a wow = new Cl88a(num1, num2);
+        wow.calc();
+
+        int sum = wow.getMySum();
+        int diff = wow.getMyDiff()
     }
 }
