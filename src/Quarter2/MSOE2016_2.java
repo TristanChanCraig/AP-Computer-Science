4098bcee54e2c1bea4beb99d23ba9e6e58e8064a
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-11-13 11:23:39 -0600

    update

--- src/Quarter2/MSOE2016_2.java
+++ src/Quarter2/MSOE2016_2.java
@@ -6,19 +6,20 @@ public class MSOE2016_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What is the length of side a?");
+        System.out.print("What is the length of side a? ");
         int a = input.nextInt();
 
-        System.out.print("What is the length of side c?");
+        System.out.print("What is the length of side c? ");
         int c = input.nextInt();
 
-        System.out.print("What is the degree measure of angle a?");
+        System.out.print("What is the degree measure of angle a? ");
         double aA = input.nextInt();
 
-        double aC = Math.sin(Math.sin(a)/a*c);
+        double aC = Math.asin((Math.sin(Math.toRadians(aA))/a)*c);
 
         double sV = 0;
         if (aA + aC < 180) sV = 180 - aC;
 
+        System.out.println(aC + " " + sV);
     }
 }
--- src/Quarter2/MSOE2016_3.java
+++ src/Quarter2/MSOE2016_3.java
@@ -3,5 +3,6 @@ import java.util.*;
 
 public class MSOE2016_3 {
     public static void main(String[] args) {
+
     }
 }
