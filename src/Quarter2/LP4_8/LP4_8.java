0ba530e1ad8a2ee3d6bef00fb2c2f6b341c97d6d
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-11-28 11:24:05 -0600

    update

--- src/Quarter2/LP4_8/LP4_8.java
+++ src/Quarter2/LP4_8/LP4_8.java
@@ -6,6 +6,9 @@ public class LP4_8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter in the seed integer: ");
+        int X0 = input.nextInt();
+
         System.out.print("Enter in value a: ");
         final int a = input.nextInt();
 
@@ -15,14 +18,10 @@ public class LP4_8 {
         System.out.print("Enter in value m: ");
         final int m = input.nextInt();
 
-        System.out.print("Enter in the seed integer: ");
-        int X0 = input.nextInt();
-
         System.out.printf("Seed = %d, a = %d, c = %d, m = %d\n\n", X0, a, c, m);
 
+        Cl4_8 bru = new Cl4_8(a, c, m, X0);
         for (int lcv = 0; lcv < 10; lcv++) {
-            Cl4_8 bru = new Cl4_8(a, c, m, X0);
-            X0 = bru.nextInt();
             System.out.println(bru.toString());
         }
 
--- src/Quarter2/Prog410t/Cl410t.java
+++ src/Quarter2/Prog410t/Cl410t.java
@@ -1,4 +1,28 @@
 package Quarter2.Prog410t;
 
 public class Cl410t {
+    private int myID;
+    private int myAnnIC;
+    private int myHHnum;
+    private int myHHcnt;
+    private int myPhhCnt;
+    private double myAvgPhh;
+    private double myAvgHHic;
+
+    public Cl410t(int id, int annIC, int hhNum) {
+        myID = id;
+        myAnnIC = annIC;
+        myHHnum = hhNum;
+        myHHcnt += 1;
+        myPhhCnt = 0;
+        myAvgPhh = 0;
+        myAvgHHic = 0;
+    }
+
+    public void calc() {
+        if (myAnnIC < 3750.00 + 750.00 * (myHHnum - 2)) myPhhCnt += 1;
+    }
+
+    public double getMyAvgPhh() { return myAvgPhh = 1.0 * myPhhCnt / myHHcnt; }
+    public double getMyAvgHHic() { return myAvgHHic = 0;}
 }
