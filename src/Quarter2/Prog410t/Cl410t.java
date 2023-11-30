ce617ef5ab67817d7bb3e9a3764a72fdaa0c44c8
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-11-30 12:39:57 -0600

    update

--- src/Quarter2/Prog410t/Cl410t.java
+++ src/Quarter2/Prog410t/Cl410t.java
@@ -4,32 +4,21 @@ public class Cl410t {
     private int myID;
     private int myAnnIC;
     private int myHHnum;
-    private int myHHcnt;
-    private int myPhhCnt;
-    private double myAvgPhh;
-    private int myRhhCnt;
-    private double myRhhAvgIC;
 
     public Cl410t(int id, int annIC, int hhNum) {
         myID = id;
         myAnnIC = annIC;
         myHHnum = hhNum;
-        myHHcnt += 1;
-        myPhhCnt = 0;
-        myAvgPhh = 0;
-        myRhhCnt = 0;
-        myRhhAvgIC = 0;
     }
 
-    public void calc() {
-        if (myAnnIC < 3750.00 + 750.00 * (myHHnum - 2)) myPhhCnt += 1;
-        else {
-            myRhhCnt++;
-            myRhhAvgIC += myAnnIC;
-        }
-    }
+    public int calcPhh() { if (myAnnIC < 3750.00 + 750.00 * (myHHnum - 2)) return 1; else return 0;}
 
-    public double getMyAvgPhh() { return myAvgPhh = 1.0 * myPhhCnt / myHHcnt; }
-    public double getMyRhhAvgIC() { return myRhhAvgIC = myRhhAvgIC / myRhhCnt; }
+    public int calcRhh() { if (myAnnIC >= 3750.00 + 750.00 * (myHHnum - 2)) return 1; else return 0;}
+
+    public int calcRhhTic() { if (myAnnIC >= 3750.00 + 750.00 * (myHHnum - 2)) return myAnnIC; else return 0; }
+
+    public double getMyAvgPhh(int pHHcnt, int hhCnt) { return 1.0 * pHHcnt / hhCnt; }
+    public double getMyRhhAvgIC(int rHHic, int rHHcnt) { return 1.0 * rHHic / rHHcnt; }
+
     public String toString() { return myID + "\t" + myAnnIC + "\t" + myHHnum; }
 }
--- src/Quarter2/Prog410t/Prog410t.java
+++ src/Quarter2/Prog410t/Prog410t.java
@@ -10,13 +10,33 @@ public class Prog410t {
             Scanner s1 = new Scanner(new File("Langdat/survey.dat"));
             Scanner s2 = new Scanner(new File("Langdat/survey2.dat"));
 
-            System.out.println("ID\tIncome\tMembers)
+            System.out.println("ID\tIncome\tMembers");
+
+            int hhCnt = 0;
+            int pHHcnt = 0;
+            int rHHcnt = 0;
+            int rHHic = 0;
+            double avgPhh = 0;
+            double rHHavgIC = 0;
+
             while (s1.hasNext()) {
                 Cl410t wah = new Cl410t(s1.nextInt(), s1.nextInt(), s1.nextInt());
-                wah.calc();
+                hhCnt++;
+                pHHcnt += wah.calcPhh();
+                rHHcnt += wah.calcRhh();
+                rHHic += wah.calcRhhTic();
+
+                rHHavgIC = wah.getMyRhhAvgIC(rHHic, rHHcnt);
+                avgPhh = wah.getMyAvgPhh(pHHcnt, hhCnt);
+
                 System.out.println(wah.toString());
+
             }
-            System.out.println("Households wit")
+
+            System.out.printf("Households with income exceeding an average income of %.2f", rHHavgIC);
+            System.out.printf("Percent of households below poverty level = $.2f percent", avgPhh);
+
+
 
             while (s2.hasNext()) {
 
