b7151e8549e5d107f3a998c981320f4bb822328f
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2023-12-06 11:37:45 -0600

    update

--- src/Quarter2/Prog214c/Cl214c.java
+++ src/Quarter2/Prog214c/Cl214c.java
@@ -4,11 +4,70 @@ public class Cl214c {
     private final double regularUnleaded;
     private final double premium;
     private final double highOctane;
-    private double carWash = 2;
+    private double myCarWashCost;
+    private double myGasCost;
+    private double myGallons;
+    private String myType;
+    private String myNeedCW;
+    private double myTotalDue;
+    private double myTypeCost;
 
-    public Cl214c() {
+    public Cl214c(String type, int gallons, String needCW) {
         regularUnleaded = 1359;
         premium = 1479;
         highOctane = 1429;
+        myCarWashCost = 0.00;
+        myGasCost = 0;
+        myGallons = gallons;
+        myType = type;
+        myNeedCW = needCW;
+        myTotalDue = 0;
+        myTypeCost = 0;
+    }
+
+    public void calc() {
+        if (myType.equals("R")) {
+            myTypeCost = regularUnleaded;
+        }
+        else if (myType.equals("P")) {
+            myTypeCost = premium;
+        }
+        else {
+            myTypeCost = highOctane;
+        }
+
+        myGasCost = myGallons * myTypeCost;
+
+        if (myNeedCW.equals("Y")) {
+            if (myGallons > 10) myCarWashCost = (2.00 - ((int)myGallons - 10) * 0.20);
+        }
+        myTotalDue = myGasCost + myCarWashCost;
+    }
+
+    public String getType() {
+        if (myType.equals("R")) {
+            return "Regular";
+        }
+        else if (myType.equals("P")) {
+            return "Premium";
+        }
+        else {
+            return "High Octane";
+        }
+    }
+
+    public double getGallons() { return myGallons; }
+    public double getTypeCost() { return myTypeCost; }
+    public double getGasCost() { return myGasCost; }
+    public String getNeedCw() { return myNeedCW; }
+    public double getTotalDue() { return myTotalDue; }
+
+    public String toString() {
+        return String.format("Bestest Gasoline Seller and Car Washering Company\n" +
+                "------------------------------------------------------------\n" +
+                "%s\t\t%.2f @ %.3f\n" +
+                "------------------------------------------------------------\n" +
+                "Gasoline\t\t\t\t%s\n" +
+                "Wash %s\t\t\t\t")
     }
 }
--- src/Quarter2/Prog214c/Prog214c.java
+++ src/Quarter2/Prog214c/Prog214c.java
@@ -1,6 +1,38 @@
 package Quarter2.Prog214c;
+import Quarter2.Prog602b.InternetCustomer;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Prog214c {
-    ArrayList<Integer> a = new ArrayList<Integer>();
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            try {
+                Scanner input = new Scanner(new File("Langdat/prog214c.dat"));
+
+                ArrayList<Cl214c> list = new ArrayList<Cl214c>();
+
+                while (input.hasNext()) {
+                    String type = input.next();
+                    double gallons = input.nextDouble();
+                    String needCW = input.next();
+
+                    Cl214c poopyhead = new Cl214c(type, gallons, needCW);
+                    list.add(poopyhead);
+                }
+
+                for (int lcv = 0; lcv < list.size(); lcv++) {
+                    Cl214c me = list.get(lcv);
+                    me.calc();
+                }
+
+
+
+            } catch (IOException e) {
+                System.out.println("Can't find data file!");
+            }
+        }
+    }
 }
