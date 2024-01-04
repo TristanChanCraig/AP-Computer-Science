40de04fa026c4cb1296d95015e361aa5f7071d21
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-01-04 11:23:43 -0600

    update 12/19

--- src/Quarter2/Prog470a.java
+++ src/Quarter2/Prog470a.java
@@ -11,7 +11,7 @@ public class Prog470a {
 
             int[][] mat = new int[6][5];
 
-            for (int r = 0; r < mat.length; r++)
+            for (int r = 0; r < mat.length-1; r++)
                 for (int c = 0; c < mat[0].length; c++)
                     mat[r][c] = input.nextInt();
 
