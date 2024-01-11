8dcbd9fb8246b83f0003bf522b3eb33c22846e01
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-01-11 11:24:47 -0600

    update 1/8

--- src/Quarter2/Prog465h.java
+++ src/Quarter2/Prog465h.java
@@ -33,23 +33,24 @@ public class Prog465h {
                             cnt2++;
                         }
 
-                for (int[] row : mat) {
-                    for (int num : row)
+                System.out.println("Original Matrix");
+                for (int[] r : mat) {
+                    for (int num : r)
                         System.out.print(num + "\t");
                     System.out.println();
                 }
                 System.out.println();
 
-                for (int[] row : mat2) {
-                    for (int num : row)
+                for (int[] r : mat2) {
+                    for (int num : r)
                         System.out.print(num + "\t");
                     System.out.println();
                 }
                 System.out.println();
 
-                if (cnt2 * 3 > row * col) System.out.println("The Original Matrix is Sparse");
-                else if (cnt2 * 3 < row * col) System.out.println("The Original Matrix is Abundant");
-                else System.out.println("The Original Matrix and the Sparse Matrix are Equally Efficient);
+                if (cnt2 * 3 < row * col) System.out.println("The Original Matrix is Sparse");
+                else if (cnt2 * 3 > row * col) System.out.println("The Original Matrix is Abundant");
+                else System.out.println("The Original Matrix and the Sparse Matrix are Equally Efficient");
                 System.out.println();
 
                 System.out.println();
@@ -60,3 +61,64 @@ public class Prog465h {
         }
     }
 }
+/*
+Original Matrix
+0	0	7	0	0	0
+0	0	0	0	-8	0
+0	0	0	0	0	0
+2	0	0	0	0	0
+0	0	0	0	0	0
+
+1	3	7
+2	5	-8
+4	1	2
+
+The Original Matrix is Sparse
+
+
+Original Matrix
+0	2	0	3	0	1
+8	0	4	0	1	0
+0	3	0	1	0	-7
+5	0	9	0	6	0
+0	2	0	-1	0	7
+
+1	2	2
+1	4	3
+1	6	1
+2	1	8
+2	3	4
+2	5	1
+3	2	3
+3	4	1
+3	6	-7
+4	1	5
+4	3	9
+4	5	6
+5	2	2
+5	4	-1
+5	6	7
+
+The Original Matrix is Abundant
+
+
+Original Matrix
+0	0	1	0	0	2
+3	0	0	4	0	0
+0	0	5	0	0	6
+7	0	0	8	0	0
+0	0	9	0	0	1
+
+1	3	1
+1	6	2
+2	1	3
+2	4	4
+3	3	5
+3	6	6
+4	1	7
+4	4	8
+5	3	9
+5	6	1
+
+The Original Matrix and the Sparse Matrix are Equally Efficient
+ */
--- src/Quarter2/Prog477a.java
+++ src/Quarter2/Prog477a.java
@@ -1,4 +1,41 @@
 package Quarter2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Prog477a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog477a  .dat"));
+
+            int[][] mat = new int[33][2];
+            int[][] mat2 = new int[6][3];
+//            int mA = 0;
+//            int mB = 0;
+//            int mC = 0;
+//            int mD = 0;
+//            int mF = 0;
+//            int fA = 0;
+//            int fB = 0;
+//            int fC = 0;
+//            int fD = 0;
+            while (input.hasNext()) {
+                for (int r = 0; r < mat.length; r++)
+                    for (int c = 0; c < mat[0].length; c++)
+                        mat[r][c] = input.nextInt();
+            }
+
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    if (c == 0) {
+                        if (r == 1) mat2[]
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
 }
