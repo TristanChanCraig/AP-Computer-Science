f37903f13400523a4736f1089bb75679f49ac9c3
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-01-17 11:37:26 -0600

    update 1/8

--- src/Quarter2/Prog492h.java
+++ src/Quarter2/Prog492h.java
@@ -9,9 +9,34 @@ public class Prog492h {
         try {
             Scanner input = new Scanner(new File("Langdat/prog492h.dat"));
 
-            int[][] mat = new int[30][30];
+            String[][] gen1 = new String[30][30];
+            String[][] gen2 = new String[30][30];
 
+            for (int lcv = 0; lcv < gen1.length; lcv++)
+                for (int lcv2 = 0; lcv2 < gen1[0].length; lcv2++)
+                    gen1[lcv][lcv2] = input.next();
 
+            while (true) {
+                int neighbors = 0;
+                for (int lcv = 0; lcv < gen1.length; lcv++) {
+                    for (int lcv2 = 0; lcv2 < gen1[0].length; lcv2++) {
+                        if (lcv != 0 && lcv != gen1.length - 1 && lcv2 != 0 && lcv2 != gen1[0].length - 1) {
+                            if (gen1[lcv - 1][lcv2 - 1].equals("*") ||
+                                    gen1[lcv][lcv2 - 1].equals("*") ||
+                                    gen1[lcv + 1][lcv2 - 1].equals("*") ||
+                                    gen1[lcv - 1][lcv2].equals("*") ||
+                                    gen1[lcv + 1][lcv2].equals("*") ||
+                                    gen1[lcv - 1][lcv2 + 1].equals("*") ||
+                                    gen1[lcv][lcv2 + 1].equals("*") ||
+                                    gen1[lcv + 1][lcv + 1].equals("*")) neighbors++;
+                            if (neighbors == 3) gen2[lcv][lcv2] = "*";
+                            else if (neighbors <= 1) gen2[lcv][lcv2] = ".";
+                            else if (neighbors >= 4) gen2[lcv][lcv2] = ".";
+                        }
+                        else gen2[lcv][lcv2] = ".";
+                    }
+                }
+            }
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
@@ -24,4 +49,3 @@ public class Prog492h {
 
 
 
- */
