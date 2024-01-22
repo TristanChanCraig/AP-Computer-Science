7d970631be40631f148b81b86c517809c17ebba6
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-01-22 11:25:02 -0600

    update 1/8

--- src/Quarter2/Prog492h.java
+++ src/Quarter2/Prog492h.java
@@ -13,39 +13,50 @@ public class Prog492h {
             String[][] gen1 = new String[30][30];
             String[][] gen2 = new String[30][30];
 
+            ArrayList<String> str = new ArrayList<>();
             for (int lcv = 0; lcv < gen1.length; lcv++) {
-                ArrayList<String> str = new ArrayList<>();
                 String apple = input.nextLine();
                 str.add(apple);
                 for (int lcv2 = 0; lcv2 < gen1[0].length; lcv2++) {
                     gen1[lcv][lcv2] = str.get(lcv).substring(lcv2, lcv2+1);
                 }
             }
-            while (true) {
-                int neighbors = 0;
-                for (int lcv = 0; lcv < gen1.length; lcv++) {
-                    for (int lcv2 = 0; lcv2 < gen1[0].length; lcv2++) {
-                        if (lcv != 0 && lcv != gen1.length - 1 && lcv2 != 0 && lcv2 != gen1[0].length - 1) {
-                            if (gen1[lcv - 1][lcv2 - 1].equals("*") ||
-                                    gen1[lcv][lcv2 - 1].equals("*") ||
-                                    gen1[lcv + 1][lcv2 - 1].equals("*") ||
-                                    gen1[lcv - 1][lcv2].equals("*") ||
-                                    gen1[lcv + 1][lcv2].equals("*") ||
-                                    gen1[lcv - 1][lcv2 + 1].equals("*") ||
-                                    gen1[lcv][lcv2 + 1].equals("*") ||
-                                    gen1[lcv + 1][lcv + 1].equals("*")) neighbors++;
-                            if (neighbors == 3) gen2[lcv][lcv2] = "*";
-                            else if (neighbors <= 1) gen2[lcv][lcv2] = ".";
-                            else if (neighbors >= 4) gen2[lcv][lcv2] = ".";
-                        }
+
+            for (String[] cells : gen1) {
+                for (String cell : cells)
+                    System.out.print(cell + " ");
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            int neighbors = 0;
+            for (int lcv = 0; lcv < gen1.length; lcv++) {
+                for (int lcv2 = 0; lcv2 < gen1[0].length; lcv2++) {
+                    neighbors = 0;
+                    if (lcv != 0 && lcv != gen1.length - 1 && lcv2 != 0 && lcv2 != gen1[0].length - 1) {
+                        if (gen1[lcv - 1][lcv2 - 1].equals("*")) neighbors++;
+                        if (gen1[lcv][lcv2 - 1].equals("*")) neighbors++;
+                        if (gen1[lcv + 1][lcv2 - 1].equals("*")) neighbors++;
+                        if (gen1[lcv - 1][lcv2].equals("*")) neighbors++;
+                        if (gen1[lcv + 1][lcv2].equals("*")) neighbors++;
+                        if (gen1[lcv - 1][lcv2 + 1].equals("*")) neighbors++;
+                        if (gen1[lcv][lcv2 + 1].equals("*")) neighbors++;
+                        if (gen1[lcv + 1][lcv + 1].equals("*")) neighbors++;
+                        if (neighbors == 3) gen2[lcv][lcv2] = "*";
+                        else if (neighbors == 2 && gen1[lcv][lcv2].equals("*")) gen2[lcv][lcv2] = "*";
+                        else if (neighbors <= 1) gen2[lcv][lcv2] = ".";
                         else gen2[lcv][lcv2] = ".";
                     }
+                    else
+                        gen2[lcv][lcv2] = ".";
                 }
-                for (String cells[] : gen2) {
-                    for (String cell : cells)
-                        System.out.println(cell + "\t");
-                    System.out.println();
-                }
+            }
+            for (String[] cells : gen2) {
+                for (String cell : cells)
+                    System.out.print(cell + " ");
+                System.out.println();
             }
 
         } catch (IOException e) {
