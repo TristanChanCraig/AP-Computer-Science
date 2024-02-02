8f79a1ad93cf79d019772b3995ab94c40599b8a3
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-02-02 10:56:46 -0600

    update 1/31

--- src/Quarter3/Prog703s/Prog703s.java
+++ src/Quarter3/Prog703s/Prog703s.java
@@ -26,7 +26,7 @@ public class Prog703s {
                     String name = input.next();
                     int number = input.nextInt();
                     double val = input.nextDouble();
-                    int wvNumber = input.nextInt();
+                    double wvNumber = input.nextDouble();
                     Computer c = new Windows(name, number, val, wvNumber);
                     list.add(c);
                 } else if (num == 3) {
@@ -35,6 +35,7 @@ public class Prog703s {
                     double val = input.nextDouble();
                     int code = input.nextInt();
                     Computer c = new Linux(name, number, val, code);
+                    list.add(c);
                 }
                 if (input.hasNext()) num = input.nextInt();
             }
@@ -51,14 +52,29 @@ public class Prog703s {
                     if (((Apple)c).getColor().equals("Green")) greenCnt++;
                     else if (((Apple)c).getColor().equals("Gold")) goldCnt++;
                 } else if (c instanceof Windows) {
-                    if (((Windows)c).getWVNumber() == 7) v7cnt++;
+                    if (((Windows)c).getWVNumber() == 7.0) v7cnt++;
                 } else if (c instanceof Linux) {
-
+                    int code2 = ((Linux)c).getCode();
+                    while (code2 > 10) {code2 %= 10;}
+                    if (code2 == 5) last5cnt++;
                 }
             }
+
+            System.out.println("Number of Green Apple Computers: " + greenCnt);
+            System.out.println("Number of Gold Apple Computers: " + goldCnt);
+
+            System.out.println("Number of Windows Machines on Version 7.0: " + v7cnt);
+
+            System.out.println("Number of Linux Machines Hava a Secret Code Number Ending in 5: " + last5cnt);
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
         }
     }
 }
+/*
+Number of Green Apple Computers: 1
+Number of Gold Apple Computers: 0
+Number of Windows Machines on Version 7.0: 2
+Number of Linux Machines Hava a Secret Code Number Ending in 5: 4
+ */
