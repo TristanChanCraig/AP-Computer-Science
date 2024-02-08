83016848f809a0cb4da68c6a22bf54b9d824f3cf
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-02-08 12:36:13 -0600

    update 2/8

--- src/Quarter3/SearchAlgs.java
+++ src/Quarter3/SearchAlgs.java
@@ -10,20 +10,22 @@ public class SearchAlgs {
         int low = 0;
         int high = array.length - 1;
         while (low <= high) {
-            int mid = low + (high-low)/2;
+            int mid = low + (high - low) / 2;
             if (array[mid] == x) return mid;
             if (array[mid] < x) low = mid + 1;
-            else high = mid - 1
+            else high = mid - 1;
         }
         return -1;
     }
 
-    /** Call with binarySearchRec(array, x, 0, array.length-1) */
+    /**
+     * Call with binarySearchRec(array, x, 0, array.length-1)
+     */
     public static int binarySearchRec(int[] array, int x, int low, int high) {
         if (low > high) return -1;
         int mid = (low + high) / 2;
         if (array[mid] == x) return mid;
-        else if (array[mid] > x) return binarySearchRec(array, x, low, mid-1);
-        return binarySearchRec(array, x, mid+1, high);
+        else if (array[mid] > x) return binarySearchRec(array, x, low, mid - 1);
+        return binarySearchRec(array, x, mid + 1, high);
     }
 }
