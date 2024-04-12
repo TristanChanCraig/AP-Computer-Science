87703d0b30e85047bafd1b080038dd1a4f17c8d5
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-04-12 11:25:59 -0500

    update 4/12/24

--- src/Quarter4/VirtualPetLab/PetManager.java
+++ src/Quarter4/VirtualPetLab/PetManager.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 public class PetManager {
     private ArrayList<Pet> pets = new ArrayList<>();
 
-    // TODO: Implement methods as needed to interact with the pets list
+    // Implement methods as needed to interact with the pets list
     public void addPet(Pet pet) { pets.add(pet); }
     public void removePet(Pet pet) { pets.remove(pet); }
     public void listPets() { for (Pet pet : pets) System.out.print(pet.getName() + " "); }
+    public ArrayList<Pet> getListPets() { return pets; }
     public int getNumPets() { return pets.size(); }
     public Pet getSelectedPet(int lcv) { return pets.get(lcv); }
 
