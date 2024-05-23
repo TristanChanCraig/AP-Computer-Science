fce272d4a04edabbce1e40078258f0ce131e5bae
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-05-23 11:20:02 -0500

    update 5/23/2024

--- src/Quarter4/Prog7300p/Prog7300p.java
+++ src/Quarter4/Prog7300p/Prog7300p.java
@@ -107,3 +107,108 @@ public class Prog7300p {
         System.out.println();
     }
 }
+/*
+AND Neuron:
+Epoch 1: Loss: 0.0
+Epoch 2: Loss: 0.25
+Epoch 3: Loss: 0.25
+Epoch 4: Loss: 0.25
+Epoch 5: Loss: 0.25
+Epoch 6: Loss: 0.25
+Epoch 7: Loss: 0.25
+Epoch 8: Loss: 0.25
+Epoch 9: Loss: 0.25
+Epoch 10: Loss: 0.25
+Epoch 11: Loss: 0.25
+Epoch 12: Loss: 0.0
+Epoch 13: Loss: 0.0
+Epoch 14: Loss: 0.0
+Epoch 15: Loss: 0.0
+Predicted: [0.0, 0.0, 0.0, 1.0]
+Actual: [0.0, 0.0, 0.0, 1.0]
+Weights: [0.25835938734421926, 0.12114874981029855]
+Bias: -0.2999999999999999
+OR Neuron:
+Epoch 1: Loss: 0.25
+Epoch 2: Loss: 0.0
+Epoch 3: Loss: 0.0
+Epoch 4: Loss: 0.0
+Epoch 5: Loss: 0.0
+Epoch 6: Loss: 0.0
+Epoch 7: Loss: 0.0
+Epoch 8: Loss: 0.0
+Epoch 9: Loss: 0.0
+Epoch 10: Loss: 0.0
+Epoch 11: Loss: 0.0
+Epoch 12: Loss: 0.0
+Epoch 13: Loss: 0.0
+Epoch 14: Loss: 0.0
+Epoch 15: Loss: 0.0
+Predicted: [0.0, 1.0, 1.0, 1.0]
+Actual: [0.0, 1.0, 1.0, 1.0]
+Weights: [0.45555497477677076, 0.1718274823843812]
+Bias: -0.09999999999999987
+
+NOR Neuron:
+Epoch 1: Loss: 0.0
+Epoch 2: Loss: 0.0
+Epoch 3: Loss: 0.0
+Epoch 4: Loss: 0.0
+Epoch 5: Loss: 0.0
+Epoch 6: Loss: 0.0
+Epoch 7: Loss: 0.0
+Epoch 8: Loss: 0.0
+Epoch 9: Loss: 0.0
+Epoch 10: Loss: 0.0
+Epoch 11: Loss: 0.0
+Epoch 12: Loss: 0.0
+Epoch 13: Loss: 0.0
+Epoch 14: Loss: 0.0
+Epoch 15: Loss: 0.0
+Predicted: [1.0, 1.0, 1.0, 0.0]
+Actual: [1.0, 1.0, 1.0, 0.0]
+Weights: [-0.2444237202858258, -0.18447476124531512]
+Bias: 0.40000000000000013
+
+NOT Neuron:
+Epoch 1: Loss: 0.5
+Epoch 2: Loss: 0.5
+Epoch 3: Loss: 0.0
+Epoch 4: Loss: 0.0
+Epoch 5: Loss: 0.0
+Epoch 6: Loss: 0.0
+Epoch 7: Loss: 0.0
+Epoch 8: Loss: 0.0
+Epoch 9: Loss: 0.0
+Epoch 10: Loss: 0.0
+Epoch 11: Loss: 0.0
+Epoch 12: Loss: 0.0
+Epoch 13: Loss: 0.0
+Epoch 14: Loss: 0.0
+Epoch 15: Loss: 0.0
+Predicted: [1.0, 0.0]
+Actual: [1.0, 0.0]
+Weights: [-0.36853364307624925]
+Bias: 0.20000000000000015
+
+NAND Neuron:
+Epoch 1: Loss: 0.25
+Epoch 2: Loss: 0.0
+Epoch 3: Loss: 0.0
+Epoch 4: Loss: 0.0
+Epoch 5: Loss: 0.0
+Epoch 6: Loss: 0.0
+Epoch 7: Loss: 0.0
+Epoch 8: Loss: 0.0
+Epoch 9: Loss: 0.0
+Epoch 10: Loss: 0.0
+Epoch 11: Loss: 0.0
+Epoch 12: Loss: 0.0
+Epoch 13: Loss: 0.0
+Epoch 14: Loss: 0.0
+Epoch 15: Loss: 0.0
+Predicted: [1.0, 0.0, 0.0, 0.0]
+Actual: [1.0, 0.0, 0.0, 0.0]
+Weights: [-0.08833327370478594, -0.025105061418474878]
+Bias: 1.3877787807814457E-16
+ */
