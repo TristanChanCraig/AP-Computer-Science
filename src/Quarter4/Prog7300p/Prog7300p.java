616a3f6b07bddc6231181c49f5f09d67be2f0c17
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-05-23 11:16:01 -0500

    update 5/23/2024

--- src/Quarter4/Prog7300p/Prog7300p.java
+++ src/Quarter4/Prog7300p/Prog7300p.java
@@ -28,13 +28,82 @@ public class Prog7300p {
 
         // Make neurons for OR, NOT, NAND, and NOR
 
+        // OR
+        X_train = new double[][]{
+                {0, 0},
+                {0, 1},
+                {1, 0},
+                {1, 1}
+        };
+        y_train = new double[]{0, 1, 1, 1};
 
+        System.out.println("OR Neuron: ");
+        model.fit(X_train, y_train);
+
+        y_pred = model.predict(X_train);
+        System.out.println("Predicted: " + Arrays.toString(y_pred));
+        System.out.println("Actual: " + Arrays.toString(y_train));
+
+        System.out.println("Weights: " + Arrays.toString(model.getWeights()));
+        System.out.println("Bias: " + model.getBias());
+        System.out.println();
+
+        // NOR dataset
+        X_train = new double[][]{
+                {0, 0},
+                {0, 1},
+                {1, 0},
+                {1, 1}
+        };
+        y_train = new double[]{1, 1, 1, 0};
+
+        System.out.println("NOR Neuron: ");
+        model.fit(X_train, y_train);
+
+        y_pred = model.predict(X_train);
+        System.out.println("Predicted: " + Arrays.toString(y_pred));
+        System.out.println("Actual: " + Arrays.toString(y_train));
+
+        System.out.println("Weights: " + Arrays.toString(model.getWeights()));
+        System.out.println("Bias: " + model.getBias());
+        System.out.println();
 
         // NOT dataset
         X_train = new double[][]{
                 {0},
                 {1}
         };
-        y_train = new double[]{1,0};
+        y_train = new double[]{1, 0};
+
+        System.out.println("NOT Neuron: ");
+        model.fit(X_train, y_train);
+
+        y_pred = model.predict(X_train);
+        System.out.println("Predicted: " + Arrays.toString(y_pred));
+        System.out.println("Actual: " + Arrays.toString(y_train));
+
+        System.out.println("Weights: " + Arrays.toString(model.getWeights()));
+        System.out.println("Bias: " + model.getBias());
+        System.out.println();
+
+        // NAND dataset
+        X_train = new double[][]{
+                {0, 0},
+                {0, 1},
+                {1, 0},
+                {1, 1}
+        };
+        y_train = new double[]{1, 0, 0, 0};
+
+        System.out.println("NAND Neuron: ");
+        model.fit(X_train, y_train);
+
+        y_pred = model.predict(X_train);
+        System.out.println("Predicted: " + Arrays.toString(y_pred));
+        System.out.println("Actual: " + Arrays.toString(y_train));
+
+        System.out.println("Weights: " + Arrays.toString(model.getWeights()));
+        System.out.println("Bias: " + model.getBias());
+        System.out.println();
     }
 }
