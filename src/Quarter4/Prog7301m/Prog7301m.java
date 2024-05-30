d97e0404cdc124e4841f8a7f280e6db2858c3de2
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-05-30 11:23:29 -0500

    update 5/30/2024

--- src/Quarter4/Prog7301m/Prog7301m.java
+++ src/Quarter4/Prog7301m/Prog7301m.java
@@ -44,6 +44,12 @@ public class Prog7301m {
         return array;
     }
 
+    public static void normalize(double[][] data, double max) {
+        for (int r = 0; r < data.length; r++)
+            for (int c = 0; c < data[0].length; c++)
+                data[r][c] /= max;
+    }
+
     public static void main(String[] args) {
         final int N_CLASSES = 10;
 
@@ -63,6 +69,9 @@ public class Prog7301m {
         double[][] y_test = toArray(yte);
 
         final int N_INPUTS = X_train[0].length;
+        final double MAX_VAL = 255.0;
+        normalize(X_train, MAX_VAL);
+        normalize(X_test, MAX_VAL);
 
         // Create model
         var model = new MultiLayerPerceptron(new int[]{N_INPUTS, 100, N_CLASSES}, 0.1, new Activations.Sigmoid());
@@ -70,7 +79,7 @@ public class Prog7301m {
         System.out.println("Final Accuracy: " + model.accuracy(X_test, y_test));
 
         // Pick a random image from the test set (validation)
-        int index = (int)(Math.random() + X_test.length) - 1;
+        int index = (int)(Math.random() * X_test.length);
         double[] X_val = X_test[index];
         double[] y_val = y_test[index];
 
@@ -82,7 +91,7 @@ public class Prog7301m {
 
         // Display the image
         var img = new BufferedImage(28, 28, BufferedImage.TYPE_INT_RGB);
-        for (int r = 0; r < 28; r++) for (int c = 0; c < 28; c++) img.setRGB(c, r, (int)(X_val[r * 28 + c]));
+        for (int r = 0; r < 28; r++) for (int c = 0; c < 28; c++) img.setRGB(c, r, (int)(X_val[r * 28 + c] * MAX_VAL));
 
         var frame = new JFrame("Image");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
